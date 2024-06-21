78153fe16b8666cdfab25e5cdd55e652744b4911
Zachary Kent <ztkent@gmail.com>
2024-06-21 00:16:22 -0400

    agent wrapper

--- pitooth/agent.go
+++ pitooth/agent.go
@@ -11,7 +11,77 @@ type PiToothAgent struct {
 	*agent.SimpleAgent
 }
 
+func (a *PiToothAgent) AuthorizeService(device dbus.ObjectPath, uuid string) *dbus.Error {
+	fmt.Println("AuthorizeService called")
+	return a.SimpleAgent.AuthorizeService(device, uuid)
+}
+
+func (a *PiToothAgent) Cancel() *dbus.Error {
+	fmt.Println("Cancel called")
+	return a.SimpleAgent.Cancel()
+}
+
+func (a *PiToothAgent) DisplayPasskey(device dbus.ObjectPath, passkey uint32, entered uint16) *dbus.Error {
+	fmt.Println("DisplayPasskey called")
+	return a.SimpleAgent.DisplayPasskey(device, passkey, entered)
+}
+
+func (a *PiToothAgent) DisplayPinCode(device dbus.ObjectPath, pincode string) *dbus.Error {
+	fmt.Println("DisplayPinCode called")
+	return a.SimpleAgent.DisplayPinCode(device, pincode)
+}
+
+func (a *PiToothAgent) Interface() string {
+	fmt.Println("Interface called")
+	return a.SimpleAgent.Interface()
+}
+
+func (a *PiToothAgent) PassCode() string {
+	fmt.Println("PassCode called")
+	return a.SimpleAgent.PassCode()
+}
+
+func (a *PiToothAgent) PassKey() uint32 {
+	fmt.Println("PassKey called")
+	return a.SimpleAgent.PassKey()
+}
+
+func (a *PiToothAgent) Path() dbus.ObjectPath {
+	fmt.Println("Path called")
+	return a.SimpleAgent.Path()
+}
+
+func (a *PiToothAgent) Release() *dbus.Error {
+	fmt.Println("Release called")
+	return a.SimpleAgent.Release()
+}
+
+func (a *PiToothAgent) RequestAuthorization(device dbus.ObjectPath) *dbus.Error {
+	fmt.Println("RequestAuthorization called")
+	return a.SimpleAgent.RequestAuthorization(device)
+}
+
 func (a *PiToothAgent) RequestConfirmation(path dbus.ObjectPath, passkey uint32) *dbus.Error {
-	fmt.Printf("Device %s paired with passkey %d\n", path, passkey)
-	return nil
+	fmt.Println("RequestConfirmation called")
+	return a.SimpleAgent.RequestConfirmation(path, passkey)
+}
+
+func (a *PiToothAgent) RequestPasskey(path dbus.ObjectPath) (uint32, *dbus.Error) {
+	fmt.Println("RequestPasskey called")
+	return a.SimpleAgent.RequestPasskey(path)
+}
+
+func (a *PiToothAgent) RequestPinCode(path dbus.ObjectPath) (string, *dbus.Error) {
+	fmt.Println("RequestPinCode called")
+	return a.SimpleAgent.RequestPinCode(path)
+}
+
+func (a *PiToothAgent) SetPassCode(pinCode string) {
+	fmt.Println("SetPassCode called")
+	a.SimpleAgent.SetPassCode(pinCode)
+}
+
+func (a *PiToothAgent) SetPassKey(passkey uint32) {
+	fmt.Println("SetPassKey called")
+	a.SimpleAgent.SetPassKey(passkey)
 }
