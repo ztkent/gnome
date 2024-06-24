6f93819ae75db434b081962181202402b5e9daee
Zachary Kent <ztkent@gmail.com>
2024-06-24 19:44:11 -0400

    better layout

--- internal/tools/connection.go
+++ internal/tools/connection.go
@@ -14,24 +14,18 @@ import (
 	pitooth "github.com/Ztkent/pitooth"
 )
 
-/*
-	 Its essential to have a way to connect a pi to the internet, without the user having to login.
-		1. Identify if the pi is connected to the internet.
-		2. If its not, turn on the bluetooth adapter.
-		3. Allow connections to our pi via bluetooth.
-		4. Turn on the obexd service.
-		5. Connect to the pi from the client (Via the app)
-		6. Send the wifi credentials to the pi, via OPP (Object Push Profile)
-		7. Attempt to connect to the wifi network using provided credentials.
-*/
 const (
 	TRANSFER_DIRECTORY = "/home/sunlight/sunlight-meter/transfers"
 )
 
+type Credentials struct {
+	SSID     string `json:"ssid"`
+	Password string `json:"password"`
+}
+
 func ManageInternetConnection() {
 	if !checkInternetConnection("") {
 		log.Println("No internet connection detected. Starting WIFI management...")
-		// Create a new Bluetooth manager
 		btm, err := pitooth.NewBluetoothManager("SunlightMeter")
 		if err != nil {
 			log.Println("Failed to create Bluetooth manager:", err)
@@ -39,7 +33,7 @@ func ManageInternetConnection() {
 		}
 		defer btm.Close(true)
 
-		// Get the client to connect to the pi w/ bluetooth
+		// Accept Bluetooth connections
 		log.Printf("Attempting to accept Bluetooth connections\n")
 		for attempt := 1; attempt <= 5; attempt++ {
 			connectedDevices, err := btm.AcceptConnections(30 * time.Second)
@@ -52,7 +46,7 @@ func ManageInternetConnection() {
 			}
 		}
 
-		// Start the OBEX server, accept file transfers
+		// Accept OBEX file transfers
 		log.Println("Starting OBEX server")
 		if err := btm.ControlOBEXServer(true, TRANSFER_DIRECTORY); err != nil {
 			log.Println("Failed to start OBEX server:", err)
@@ -60,7 +54,7 @@ func ManageInternetConnection() {
 		}
 		defer btm.ControlOBEXServer(false, "")
 
-		// Watch /transfers for new files
+		// Watch for new credentials
 		creds, err := watchForCreds(time.Second * 180)
 		if err != nil {
 			log.Println("Failed to receive wifi credentials:", err)
@@ -70,9 +64,8 @@ func ManageInternetConnection() {
 			return
 		}
 
+		// If we got credentials, add them to wpa_supplicant.conf
 		for _, creds := range creds {
-			// Connect to the Wi-Fi network
-			log.Println("Attempting to add Wi-Fi network to wpa_supplicant.conf: ", creds.SSID, creds.Password)
 			if err := addWifiNetwork(creds.SSID, creds.Password); err != nil {
 				log.Println("Failed to add Wi-Fi network:", err)
 				return
@@ -80,7 +73,7 @@ func ManageInternetConnection() {
 		}
 		logWpaSupplicantContents()
 
-		// Restart the networking service
+		// Attempt to restart networking service
 		log.Println("Restarting networking service")
 		cmd := exec.Command("systemctl", "restart", "networking")
 		if err := cmd.Run(); err != nil {
@@ -88,11 +81,13 @@ func ManageInternetConnection() {
 			return
 		}
 
-		// Check if the Pi is connected to the internet
+		// Restarting the networking service might connect to the Wi-Fi network
 		if !checkInternetConnection("http://www.google.com") {
 			log.Println("Failed to connect to Wi-Fi network")
 			return
 		}
+
+		// Log the SSID we're connected to
 		currentSSID, err := getCurrentSSID()
 		if err != nil {
 			log.Println("Failed to get current SSID:", err)
@@ -100,12 +95,61 @@ func ManageInternetConnection() {
 			log.Printf("Successfully connected to Wi-Fi network: %s\n", currentSSID)
 		}
 	}
-	return
+}
+
+func checkInternetConnection(testSite string) bool {
+	client := http.Client{
+		Timeout: 10 * time.Second,
+	}
+	if testSite == "" {
+		testSite = "http://www.ztkent.com"
+	}
+	log.Println("Checking internet connection: ", testSite)
+	response, err := client.Get(testSite)
+	if err != nil {
+		return false
+	}
+	defer response.Body.Close()
+	connected := response.StatusCode == 200
+	if connected {
+		ssid, err := getCurrentSSID()
+		if err != nil {
+			log.Println("Failed to get current SSID:", err)
+		} else {
+			log.Println("Connected to Wi-Fi network:", ssid)
+		}
+	} else {
+		log.Println("Not connected to the internet")
+	}
+	return connected
+}
+
+func getCurrentSSID() (string, error) {
+	cmd := exec.Command("iwgetid", "-r")
+	output, err := cmd.Output()
+	if err != nil {
+		return "", err
+	}
+	ssid := strings.TrimSpace(string(output))
+	return ssid, nil
+}
+
+func addWifiNetwork(ssid, password string) error {
+	log.Println("Attempting to add Wi-Fi network to wpa_supplicant.conf: ", ssid, password)
+	file, err := os.OpenFile("/etc/wpa_supplicant/wpa_supplicant.conf", os.O_APPEND|os.O_WRONLY, 0644)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+	networkConfig := fmt.Sprintf("\nnetwork={\n    ssid=\"%s\"\n    psk=\"%s\"\n    key_mgmt=WPA-PSK\n}\n", ssid, password)
+	if _, err = file.WriteString(networkConfig); err != nil {
+		return err
+	}
+	return nil
 }
 
 func watchForCreds(timeout time.Duration) ([]*Credentials, error) {
 	log.Println("Watching for new files in ", TRANSFER_DIRECTORY)
-
 	timeoutTimer := time.NewTimer(timeout)
 	retryTicker := time.NewTicker(5 * time.Second)
 	defer retryTicker.Stop()
@@ -114,34 +158,16 @@ func watchForCreds(timeout time.Duration) ([]*Credentials, error) {
 		case <-timeoutTimer.C:
 			return nil, fmt.Errorf("Timed out waiting for credentials")
 		case <-retryTicker.C:
-			var err error
 			creds, err := processDirectory(TRANSFER_DIRECTORY)
 			if err != nil {
 				log.Println("Error processing directory:", err)
 				return nil, err
 			}
 			if creds != nil {
-				// Credentials found, stop watching
 				return creds, nil
 			}
 		}
 	}
-}
-
-func cleanUpTransfers() {
-	log.Println("Cleaning up transfers directory of .creds files")
-	files, _ := filepath.Glob(filepath.Join(TRANSFER_DIRECTORY, "*.creds"))
-	for _, file := range files {
-		if err := os.Remove(file); err != nil {
-			log.Println("Failed to delete .creds file:", file, err)
-		}
-	}
-	return
-}
-
-type Credentials struct {
-	SSID     string `json:"ssid"`
-	Password string `json:"password"`
 }
 
 func processDirectory(dirPath string) ([]*Credentials, error) {
@@ -149,7 +175,6 @@ func processDirectory(dirPath string) ([]*Credentials, error) {
 	if err != nil {
 		return nil, err
 	}
-	// Setup for transfers by cleaning the transfers directory of any existing .creds files
 	cleanUpTransfers()
 
 	foundCreds := []*Credentials{}
@@ -165,7 +190,6 @@ func processDirectory(dirPath string) ([]*Credentials, error) {
 				continue
 			}
 			if creds.SSID != "" && creds.Password != "" {
-				log.Printf("Found credentials in %s: %+v\n", fullPath, creds)
 				foundCreds = append(foundCreds, creds)
 			}
 		}
@@ -188,65 +212,14 @@ func readCredentials(filePath string) (*Credentials, error) {
 	return &creds, nil
 }
 
-func checkInternetConnection(testSite string) bool {
-	client := http.Client{
-		Timeout: 10 * time.Second,
-	}
-	if testSite == "" {
-		testSite = "http://www.ztkent.com"
-	}
-	response, err := client.Get(testSite)
-	if err != nil {
-		return false
-	}
-	defer response.Body.Close()
-	connected := response.StatusCode == 200
-	if connected {
-		ssid, err := getCurrentSSID()
-		if err != nil {
-			log.Println("Failed to get current SSID:", err)
-		} else {
-			log.Println("Connected to Wi-Fi network:", ssid)
+func cleanUpTransfers() {
+	log.Println("Cleaning up transfers directory of .creds files")
+	files, _ := filepath.Glob(filepath.Join(TRANSFER_DIRECTORY, "*.creds"))
+	for _, file := range files {
+		if err := os.Remove(file); err != nil {
+			log.Println("Failed to delete .creds file:", file, err)
 		}
-	} else {
-		log.Println("Not connected to the internet")
 	}
-
-	return connected
-}
-
-func getCurrentSSID() (string, error) {
-	cmd := exec.Command("iwgetid", "-r")
-	output, err := cmd.Output()
-	if err != nil {
-		return "", err
-	}
-	ssid := strings.TrimSpace(string(output))
-	return ssid, nil
-}
-
-// AddWifiNetwork adds a Wi-Fi network configuration to the wpa_supplicant.conf file.
-func addWifiNetwork(ssid, password string) error {
-	file, err := os.OpenFile("/etc/wpa_supplicant/wpa_supplicant.conf", os.O_APPEND|os.O_WRONLY, 0644)
-	if err != nil {
-		return err
-	}
-	defer file.Close()
-	networkConfig := fmt.Sprintf("\nnetwork={\n    ssid=\"%s\"\n    psk=\"%s\"\n    key_mgmt=WPA-PSK\n}\n", ssid, password)
-	if _, err = file.WriteString(networkConfig); err != nil {
-		return err
-	}
-	return nil
-}
-
-// RemoveWifiNetwork removes a Wi-Fi network configuration from the wpa_supplicant.conf file based on the SSID.
-func removeWifiNetwork(ssid string) error {
-	cmdStr := fmt.Sprintf("/network={/,/}/ { /ssid=\"%s\"/,/}/d }", ssid)
-	cmd := exec.Command("sed", "-i", cmdStr, "/etc/wpa_supplicant/wpa_supplicant.conf")
-	if err := cmd.Run(); err != nil {
-		return fmt.Errorf("failed to remove Wi-Fi network: %w", err)
-	}
-	return nil
 }
 
 func logWpaSupplicantContents() {
